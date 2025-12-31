ba0754d6fe15779b49965c76674c156d9c38bd0f
Julio <julio@example.com>
2025-12-30 21:52:17 -0300

    Remove MongoDB initialization to fix crash loop

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,16 @@
-import Sequelize from "sequelize";
-import mongoose from "mongoose";
-
-import configDatabase from "../config/database.js";
-
-import User from "../app/models/User.js";
-import Product from "../app/models/Product.js";
-import Category from "../app/models/Category.js";
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+import configDatabase from '../config/database.js';
+import User from '../app/models/User.js';
+import Product from '../app/models/Product.js';
+import Category from '../app/models/Category.js';
 
 const models = [User, Product, Category];
 
 class Database {
   constructor() {
     this.init();
-    this.mongo();
+    // REMOVIDO: this.mongo();
   }
 
   init() {
@@ -23,10 +21,10 @@ class Database {
         (model) => model.associate && model.associate(this.connection.models)
       );
   }
-  mongo() {
-    this.mongoConnection = mongoose.connect(process.env.MONGO_URL) ;
-  }
+
+  // REMOVIDO: método mongo()
 }
 
 export default new Database();
+
 
