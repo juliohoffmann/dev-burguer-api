11083ddf64953ec9eda4403b7307274a3c86fdfe
Julio <julio@example.com>
2026-01-01 19:35:55 -0300

    modified controllers/SessionController.js

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,8 +1,8 @@
 import * as Yup from 'yup';
 import User from '../models/User.js';
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import authConfig from './../../config/auth.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import authConfig from './../../config/auth.js';
 
 class SessionController {
     async store(request, response) {
