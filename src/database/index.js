3a567a480a1c363b8dcabb8f38c9a3de4a8f2b65
Julio <julio@example.com>
2026-01-02 10:43:03 -0300

    modified database/index

--- src/database/index.js
+++ src/database/index.js
@@ -1,28 +1,31 @@
-import { Sequelize } from "sequelize";
-import mongoose from "mongoose";
-import databaseConfig from "../config/database.cjs";
-import User from "../app/models/User.js";
-import Product from "../app/models/Product.js";
-import Category from "../app/models/Category.js";
+    // src/database/index.js
+    import { Sequelize } from "sequelize";
+    import mongoose from "mongoose";
+    import databaseConfig from "../config/database.cjs";
+    import User from "../app/models/User.js";
+    import Product from "../app/models/Product.js";
+    import Category from "../app/models/Category.js";
 
-const models = [User, Product, Category];
+    const models = [User, Product, Category];
 
-class Database {
-    constructor() {
-        this.init();
-        this.mongo()
+    class Database {
+        constructor() {
+            this.init();
+            this.mongo();
+        }
+
+        init() {
+            this.connection = new Sequelize(databaseConfig);
+            models.map((model)  => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models));
+        }
+
+        mongo() {
+            // Use a variável de ambiente MONGO_URL aqui
+            this.mongooseConnection = mongoose.connect(process.env.MONGO_URL);
+        }
     }
 
-    init() {
-        this.connection = new Sequelize(databaseConfig);
-        models.map((model)  => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models));
-    }
+    export default new Database();
 
-    mongo() {
-        this.mongooseConnection = mongoose.connect('mongodb://localhost:27017/devburguer')
-    }
-}
-
-export default new Database();
 
 
