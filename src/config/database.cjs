4cf5cded4e073629c43a6a9029debb8131d3d81e
Julio <julio@example.com>
2026-01-01 21:57:05 -0300

    modified database

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,14 +1,15 @@
 module.exports = {
+  development: {
     dialect: 'postgres',
     host: 'localhost',
     port: 5433,
-    username: 'postgres',
-    password: 'postgres',
+    username: 'admin',
+    password: '123456',
     database: 'dev-burguer-db',
     define: {
-        timestamps: true, //vai trackear a data de criaçao e atualização do usuario criado
-        underscored: true,
-        underscoredAll: true,
-
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
     },
+  }
 };
