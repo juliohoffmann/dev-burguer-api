8a2f6b92977bc80f324bee86dfba77a3c8b39c51
Julio <julio@example.com>
2026-01-01 22:15:00 -0300

    modified database

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,24 +1,18 @@
-module.exports = { development: { dialect: 'postgres', host: 'localhost', port: 5433, username: 'postgres', password: 'postgres', database: 'dev-burguer-db', define: { timestamps: true, // cria colunas createdAt e updatedAt underscored: true, underscoredAll: true, }, }, test: { dialect: 'postgres', host: 'localhost', port: 5433, username: 'postgres', password: 'postgres', database: 'dev-burguer-db-test', }, production: { dialect: 'postgres', host: 'localhost', port: 5433, username: 'postgres', password: 'pomodule.exports = {
+
+module.exports = {
   development: {
-    username: "postgres",
-    password: "sua_senha",
-    database: "meubanco_dev",
-    host: "127.0.0.1",
-    dialect: "postgres"
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'dev-burguer-db',
+    define: {
+      timestamps: true, // cria colunas createdAt e updatedAt
+      underscored: true,
+      underscoredAll: true,
+    },
   },
-  test: {
-    username: "postgres",
-    password: "sua_senha",
-    database: "meubanco_test",
-    host: "127.0.0.1",
-    dialect: "postgres"
-  },
-  production: {
-    username: "postgres",
-    password: "sua_senha",
-    database: "meubanco_prod",
-    host: "127.0.0.1",
-    dialect: "postgres"
-  }
+
+
 };
-stgres', database: 'dev-burguer-db-prod', }, };
