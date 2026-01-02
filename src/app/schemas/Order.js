1951715d872894da3096a460f87908dff51f007b
Julio <julio@example.com>
2026-01-01 23:50:10 -0300

    modified schemas/Order

--- src/app/schemas/Order.js
+++ src/app/schemas/Order.js
@@ -21,7 +21,7 @@ const OrderSchema = new mongoose.Schema({
             required: true
         },
         price: {
-            type: String,
+            type: Number,
             required: true
         },
         category: {
